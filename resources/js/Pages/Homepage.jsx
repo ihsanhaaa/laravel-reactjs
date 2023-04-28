f6a88bd3fa05d29449e6ed2602f91ad884f1d8a1
ihsanhaaa <editorgabut404@gmail.com>
2023-04-29 00:59:59 +0700

    add component and parsing data

--- resources/js/Pages/Homepage.jsx
+++ resources/js/Pages/Homepage.jsx
@@ -1,25 +1,29 @@
+import PostsList from '@/Components/Homepage/PostsList';
 import Navbar from '@/Components/Navbar';
 import { Link, Head } from '@inertiajs/react';
 
 export default function Homepage(props) {
-    // console.log(props);
+    console.log(props);
     return (
         <>
-            <Head title="Beranda" />
-            <Navbar />
+            <div className='min-h-screen'>
+                <Head title="Beranda" />
+                <Navbar />
+                <PostsList posts={props.posts} />
 
-            <h1>{props.description}</h1>
+                {/* <h1>{props.description}</h1>
 
-            {props.posts ? props.posts.map((data, i) => {
-                return (
-                    <div key={i} className='p-4 m-2 bg-white text-black'>
-                        <p>{data.title}</p>
-                        <p>{data.category}</p>
-                        <p>{data.author}</p>
-                        <p>{data.content}</p>
-                    </div>
-                )
-            }): <p>tidak ada data</p> }
+                {props.posts ? props.posts.map((data, i) => {
+                    return (
+                        <div key={i} className='p-4 m-2 bg-white text-black'>
+                            <p>{data.title}</p>
+                            <p>{data.category}</p>
+                            <p>{data.author}</p>
+                            <p>{data.content}</p>
+                        </div>
+                    )
+                }): <p>tidak ada data</p> } */}
+            </div>
         </>
     );
 }
